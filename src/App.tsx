775a68b9ed026ada2963c4ad666fb9e404cc87fa
Dorcas Tan <46101221+dorcastan@users.noreply.github.com>
2021-11-08 15:43:54 +0800

    Add react-router-dom for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Routes, Route } from 'react-router';
 import Login from './pages/Login';
 
 function App(): React.ReactElement {
-  return <Login />;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
